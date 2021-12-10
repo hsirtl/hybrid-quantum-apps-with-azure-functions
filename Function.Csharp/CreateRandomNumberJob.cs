03a019c043ef7c13baec664922730675bd702e93
hsirtl <1764309+hsirtl@users.noreply.github.com>
2021-12-10 11:37:35 +0100

    initial version

---
+++ Function.Csharp/CreateRandomNumberJob.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Quantum;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Quantum.Providers.IonQ.Targets;
+using QuantumRNG;
+using System;
+using System.Threading.Tasks;
+
+namespace Function.Csharp
+{
+    public static class CreateRandomNumberJob
+    {
+        [FunctionName("CreateRandomNumberJob")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            // define the workspace that manages the target machines.
+            var workspace = new Workspace
+            (
+                subscriptionId: Environment.GetEnvironmentVariable("subscriptionId"),
+                resourceGroupName: Environment.GetEnvironmentVariable("resourceGroupName"),
+                workspaceName: Environment.GetEnvironmentVariable("workspaceName"),
+                location: Environment.GetEnvironmentVariable("location")
+            );
+
+            // Select the machine that will execute the Q# operation.
+            // Target must be enabled in the workspace referenced above.
+            var targetId = "ionq.simulator";
+
+            var quantumMachine = new IonQQuantumMachine(
+                target: targetId,
+                workspace: workspace);
+
+            // Submit the random number job to the target machine.
+            var randomNumberJob = quantumMachine.SubmitAsync(GenerateRandomBits.Info, 4);
+
+            return new OkObjectResult($"RandomBits job id: {randomNumberJob.Result.Id}");
+        }
+    }
+}
---
+++ QApp.Qsharp/Program.qs
@@ -0,0 +1,13 @@
+﻿namespace QuantumRNG {
+
+    open Microsoft.Quantum.Intrinsic;
+    open Microsoft.Quantum.Measurement;
+    open Microsoft.Quantum.Canon;
+
+    @EntryPoint()
+    operation GenerateRandomBits(n : Int) : Result[] {
+        use qubits = Qubit[n];
+        ApplyToEach(H, qubits);
+        return MultiM(qubits);
+    }
+}
